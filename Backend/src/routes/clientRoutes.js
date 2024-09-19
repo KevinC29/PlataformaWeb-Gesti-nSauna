d229ff3ddf80adc84727ecd905305a95e9045a0d
KevinC29 <solanokevyn@gmail.com>
2024-09-18 20:26:28 -0500

    Modificacion de controladores y modelos

--- Backend/src/routes/clientRoutes.js
+++ Backend/src/routes/clientRoutes.js
@@ -3,17 +3,20 @@ import {
     createClient,
     getClients,
     getClient,
+    getClientByAuthenticatedUser,
     updateClient,
-    deleteClient
-} from '../controllers/ClientController.js';
+    deleteClient,
+} from '../controllers/clientController.js';
 import checkAuth from "../utils/helpers/handleAuthCheck.js";
 
 const router = express.Router();
 
 router.post('/', checkAuth(['ADMIN', 'CASHIER']), createClient);
 router.get('/', checkAuth(['ADMIN', 'CASHIER']), getClients);
+router.get('/by-user', checkAuth(['ADMIN', 'CLIENT']), getClientByAuthenticatedUser);
 router.get('/:id', checkAuth(['ADMIN', 'CASHIER']), getClient);
 router.put('/:id', checkAuth(['ADMIN', 'CASHIER']), updateClient);
 router.delete('/:id', checkAuth(['ADMIN']), deleteClient);
+
 
 export default router; 
