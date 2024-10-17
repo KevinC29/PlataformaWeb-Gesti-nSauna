bad4d2b195673274b69ae3ef736a637c66558d76
KevinC29 <solanokevyn@gmail.com>
2024-10-17 17:29:24 -0500

    Cambios en controladores de factura

--- Backend/src/controllers/invoiceController.js
+++ Backend/src/controllers/invoiceController.js
@@ -1,39 +1,60 @@
-// facturaController.js
+import mongoose from 'mongoose';
 import { generateInvoicePDF } from "../utils/invoice/pdfInvoiceService.js";
 import { sendInvoiceEmail } from "../config/email.js";
 import handleError from "../utils/helpers/handleError.js";
-import { validateInoviceData } from '../validators/invoiceValidate.js';
-
+import { validateInvoiceData } from '../validators/invoiceValidate.js';
+import Order from '../models/orderModel.js';
+import fs from 'fs/promises';
 export const invoiceSend = async (req, res) => {
-  try {
+  let session;
 
-    const validationResult = validateInoviceData(req.body);
+  try {
+    session = await mongoose.startSession();
+    session.startTransaction();
+
+    const validationResult = validateInvoiceData(req.body);
     if (!validationResult.isValid) {
-      return handleError(res, null, null, 400, validationResult.message);
+      return handleError(res, null, session, 400, validationResult.message);
     }
 
-    const { htmlTemplate, email, subject, numberInvoice } = req.body;
+    const { htmlTemplate, email, subject, numberInvoice, orderId } = req.body;
 
-    if (!htmlTemplate || !email || !subject || !numberInvoice) {
-      return handleError(res, null, null, 400, "Datos insuficientes para crear y enviar la factura");
+    const order = await Order.findById(orderId).exec();
+    if (!order) {
+      return handleError(res, null, session, 404, "La orden no existe");
     }
 
     if (!email) {
-      return handleError(res, null, null, 500, "No cuenta con un correo para enviar");
+      return handleError(res, null, session, 500, "No cuenta con un correo para enviar");
     }
 
     // Generar el PDF de la factura
     const pdfPath = await generateInvoicePDF(htmlTemplate, numberInvoice);
-    console.log(pdfPath)
+    
     // Enviar la factura por correo electrónico
-    // const emailSent = await sendInvoiceEmail(email, pdfPath, subject);
+    const emailSent = await sendInvoiceEmail(email, pdfPath, subject);
+    if (!emailSent) {
+      return handleError(res, null, session, 500, "Error al enviar el correo con la factura adjunta");
+    }
 
-    // if (!emailSent) {
-    //   return handleError(res, null, null, 500, "Error al enviar el correo con la factura adjunta");
-    // }
+    await fs.unlink(pdfPath);
 
-    res.status(200).json({ message: "Factura creada y enviada con éxito", pdfPath });
+    const updatedOrder = await Order.findByIdAndUpdate(orderId, { isActive: false }, { new: true, session }).exec();
+
+    await session.commitTransaction();
+    res.status(200).json({ data: updatedOrder, message: "Factura creada y enviada con éxito" });
   } catch (error) {
-    handleError(res, error, null, 500, "Error al generar o enviar la factura");
+    if (session && session.inTransaction()) {
+      try {
+        await session.abortTransaction();
+      } catch (abortError) {
+        console.error('Error al abortar la transacción:', abortError);
+      }
+    }
+    handleError(res, error, session, 500, "Error al generar o enviar la factura");
+  } finally {
+    if (session) {
+      session.endSession();
+    }
   }
 };
--- Backend/src/utils/invoice/pdfInvoiceService.js
+++ Backend/src/utils/invoice/pdfInvoiceService.js
@@ -1,18 +1,34 @@
 import puppeteer from 'puppeteer';
-import path from 'path';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
-// Función para crear un PDF basado en una plantilla HTML
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const generateInvoicePDF = async (htmlTemplate, numberInvoice) => {
   try {
-    const pdfPath = path.join(__dirname, `Factura${numberInvoice}.pdf`);
-
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      headless: 'shell'
+    });
     const page = await browser.newPage();
-    await page.setContent(htmlTemplate);
-    await page.pdf({ path: pdfPath, format: 'A4', printBackground: true });
+    await page.setContent(htmlTemplate, {
+      waitUntil: 'networkidle0',
+    });
+    const pdfPath = path.join(__dirname, `NotaVenta${numberInvoice}.pdf`);
+    await page.pdf({
+      path: pdfPath,
+      format: 'A4',
+      printBackground: true,
+      margin: {
+        top: '10mm',
+        right: '10mm',
+        bottom: '10mm',
+        left: '10mm',
+      },
+    });
     await browser.close();
     return pdfPath;
   } catch (error) {
-    throw new Error("Error al crear la factura en PDF: " + error.message);
+    throw new Error(`Error al crear la factura en PDF: ${error.message}`);
   }
 };
